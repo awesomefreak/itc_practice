38c7269c5cc5ab4a30f29505f47b40472364d943
Konstantin Saprykin <saprykin.kostja@gmail.com>
2021-10-05 02:09:08 +0500

    now handle most api exceptions

--- MyGithub.py
+++ MyGithub.py
@@ -1,7 +1,35 @@
 import base64
-import json
-from urllib import response
 from MyRequests import request
+
+class MyGithubException(Exception):
+    """
+    This class is the base of all exceptions raised by MyGithub
+    """
+
+
+class RateLimitExceededException(MyGithubException):
+    """
+    Exception raised when the rate limit is exceeded (when Github API replies with a 403 rate limit exceeded HTML status)
+    """
+
+
+class BadCredentialsException(MyGithubException):
+    """
+    Exception raised in case of bad credentials (when Github API replies with a 401 or 403 HTML status)
+    """
+
+
+class BadUserAgentException(MyGithubException):
+    """
+    Exception raised when request is sent with a bad user agent header (when Github API replies with a 403 bad user agent HTML status)
+    """
+
+
+class UnknownException(MyGithubException):
+    """
+    Exception raised when no other Exception worked
+    """
+
 
 class Github:
     headers = {
@@ -31,6 +59,27 @@ class Github:
             auth_header = f"token {token}"
         self.headers['Authorization'] = auth_header
 
+    def github_request(self, url, headers, params, method):
+        response = request(url, headers=headers, params=params, method=method)
+        if response.status == 401 and response.get("message") == "Bad credentials":
+            raise BadCredentialsException
+        elif response.status == 403 and response.get("message").startswith(
+            "Missing or invalid User Agent string"
+        ):
+            raise BadUserAgentException
+        elif response.status == 403 and (
+            response.get("message").lower().startswith("api rate limit exceeded")
+            or response.get("message")
+            .lower()
+            .endswith("please wait a few minutes before you try again.")
+        ):
+            raise RateLimitExceededException
+        elif response.status == 200:
+            return response
+        else:
+            raise UnknownException
+
+
     def set_repo(self, owner, repo):
         assert owner and isinstance(owner, str)
         assert repo and isinstance(repo, str)
@@ -39,15 +88,15 @@ class Github:
 
     def get_pulls(self, params):
         url = self.base_url + f"/repos/{self.owner}/{self.repo}/pulls"
-        response = request(url, headers=self.headers, params=params, method="get")
+        response = self.github_request(url, headers=self.headers, params=params, method="get")
         return response
 
     def get_issues(self, params):
         url = self.base_url + f"/repos/{self.owner}/{self.repo}/issues"
-        response = request(url, headers=self.headers, params=params, method="get")
+        response = self.github_request(url, headers=self.headers, params=params, method="get")
         return response
 
     def get_commits(self, params):
         url = self.base_url + f"/repos/{self.owner}/{self.repo}/commits"
-        response = request(url, headers=self.headers, params=params, method="get")
+        response = self.github_request(url, headers=self.headers, params=params, method="get")
         return response
